c3a1279ee84f9d4ac6bb4db95a2a1cb5777782b2
Neko <99990146+Neko-22@users.noreply.github.com>
2025-02-02 00:24:08 -0600

    will note this code is broken atm but i should commit before i sleep soon

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,11 @@ import { glob } from "astro/loaders";
 // 3. Define your collection(s)
 const blog_posts = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/posts" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+  }),
 });
 
 // 4. Export a single `collections` object to register your collection(s)
